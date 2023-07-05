147fc9e333ef69ee2a18110f5e55ade89bde2f1d
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-05 21:23:56 +0530

    added home skills and projects active link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,8 +29,10 @@ import { Container, Navbarn } from 'react-bootstrap';
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home" className={activeLink ==='home' ? 'active navbar-link' : 'navbar-link'} onclick={()=> onUpdateActiveLink("Home")}>Home</Nav.Link>
+            <Nav.Link href="#skills"className={activeLink ==='skills' ? 'active navbar-link' : 'navbar-link'}onclick={()=> onUpdateActiveLink("Skills")}>skills</Nav.Link>
+            <Nav.Link href="#projects"className={activeLink ==='projects' ? 'active navbar-link' : 'navbar-link'}onclick={()=> onUpdateActiveLink("Projects")}>projects</Nav.Link>
+
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
